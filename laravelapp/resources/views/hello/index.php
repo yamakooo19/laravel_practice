4a7b45039cd2fdf52f9dddf08cdd4901224fd01d
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-11 03:11:16 +0900

    PHPテンプレートの作成と、ルートの設定でテンプレートを表示する。

---
+++ laravelapp/resources/views/hello/index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>Hello/Index</title>
+    <style>
+        body {
+            font-size: 16pt;
+            color: #999;
+        }
+
+        h1 {
+            font-size: 100pt;
+            text-align: right;
+            color: #eee;
+            margin: -50px 0px -100px 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Index</h1>
+    <p>This is a sample page with php-template.</p>
+</body>
+
+</html>
