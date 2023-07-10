d7f9da008eb5679df298bcaf4f218c39fc16fd7a
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-11 00:19:28 +0900

    アクションを追加する
    アクションとはコントローラ内に用意される処理のこと。（アクションメソッド）
    ここではindexアクションがreturnでHTMLコードを返しています。
    アクション作成後、アクションを使用するにはルーティング設定をして呼び出す必要がある。
    コントローラを利用する場合には、「利用するコントローラ@アクション名」と指定する。

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -57,25 +57,28 @@ Route::get('/', function () {
 //Route::getの働きとして、「HTMLコードをreturnする関数を用意すればそのままWebページが表示される」という仕組みを理解する。
 //実際に本格的なWebページを作る場合は別の方法で実装する。
 
-Route::get('/hello/{msg?}', function ($msg = 'no message.') {
+// Route::get('/hello/{msg?}', function ($msg = 'no message.') {
 
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body { font-size:16pt; color:#999; }
-    h1 { font-size:100pt; text-align:right; color:#eee;
-        margin:-40px 0px -50px 0px; }
-    </style>
-    </head>
-    <body>
-        <h1>Hello</h1>
-        <p>{$msg}</p>
-        <p>これは、サンプルで作ったページです。</p>
-    </body>
-    </html>
-    EOF;
+//     $html = <<<EOF
+//     <html>
+//     <head>
+//     <title>Hello</title>
+//     <style>
+//     body { font-size:16pt; color:#999; }
+//     h1 { font-size:100pt; text-align:right; color:#eee;
+//         margin:-40px 0px -50px 0px; }
+//     </style>
+//     </head>
+//     <body>
+//         <h1>Hello</h1>
+//         <p>{$msg}</p>
+//         <p>これは、サンプルで作ったページです。</p>
+//     </body>
+//     </html>
+//     EOF;
 
-    return $html;
-});
+//     return $html;
+// });
+
+
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
