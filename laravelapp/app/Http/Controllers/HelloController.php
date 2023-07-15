3c4d445d0c2aa121a86fef1529bbec6ac37300f7
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-15 15:20:50 +0900

    値をテンプレートに渡す

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -11,6 +11,7 @@ class HelloController extends Controller
 {
     public function index(Request $request, Response $response)
     {
-        return view('hello.index');
+        $data = ['msg' => 'これはコントローラから渡されたメッセージです。'];
+        return view('hello.index', $data);
     }
 }
