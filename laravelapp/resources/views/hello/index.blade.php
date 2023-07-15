6f2190748c581005547552139ecd8ed237f7f3b2
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-15 16:49:33 +0900

    @ifディレクティブ

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -20,7 +20,11 @@
 
 <body>
     <h1>Blade/Index</h1>
-    <p>{{ $msg }}</p>
+    @if ($msg != '')
+        <p>こんにちは、{{ $msg }}さん。</p>
+    @else
+        <p>何か書いてください。</p>
+    @endif
     <form method="POST" action="/hello">
         @csrf
         <input type="text" name="msg">
