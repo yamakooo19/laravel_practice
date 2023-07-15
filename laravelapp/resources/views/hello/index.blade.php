0d3a4f0b048844aeb41059d451e5b29fb3c42c83
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-15 23:09:50 +0900

    @phpディレクティブについて

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -21,17 +21,17 @@
 <body>
     <h1>Blade/Index</h1>
     <p>&#064;foreachディレクティブの例</p>
-    @foreach ($data as $item)
-        @if ($loop->first)
-            <p>※データ一覧</p>
-            <ul>
-        @endif
-        <li>No,{{ $loop->iteration }}. {{ $item }}</li>
-        @if ($loop->last)
-            </ul>
-            <p>----ここまで</p>
-        @endif
-    @endforeach
+    <ol>
+        @php
+            $counter = 0;
+        @endphp
+        @while ($counter < count($data))
+            <li>{{ $data[$counter] }}</li>
+            @php
+                $counter++;
+            @endphp
+        @endwhile
+    </ol>
 </body>
 
 </html>
