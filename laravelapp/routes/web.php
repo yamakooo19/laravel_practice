6dc1d8628283f52d03303b6cc349e9e381e20cbc
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-16 23:40:48 +0900

    バリデーションを利用する

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -18,9 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('hello', function(){
-//     return view('hello.index');
-// });
-Route::get('hello', 'App\Http\Controllers\HelloController@index')
-    ->middleware('helo');
-// Route::post('hello', 'App\Http\Controllers\HelloController@post');
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::post('hello', 'App\Http\Controllers\HelloController@post');
