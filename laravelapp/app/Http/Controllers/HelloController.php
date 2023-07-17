3bf8bc703dc8947967ad8c6268bb1e3f1009652e
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-17 18:46:45 +0900

    オリジナルバリデータの作成

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -24,38 +24,8 @@ class HelloController extends Controller
         return view('hello.index', ['msg' => $msg,]);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric',
-        ];
-
-        $messages = [
-            'name.required' => '名前は必ず入力してください。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で記入ください。',
-            'age.min' => '年齢は0歳以上で記入ください。',
-            'age.max' => '年齢は200歳以下で記入ください。'
-        ];
-
-        $validator = Validator::make($request->all(), $rules, $messages);
-
-        $validator->sometimes('age', 'min:0', function ($input) {
-            return is_numeric($input->age);
-        });
-
-        $validator->sometimes('age', 'max:200', function ($input) {
-            return is_numeric($input->age);
-        });
-
-        if ($validator->fails()) {
-            return redirect('/hello')
-                ->withErrors($validator)
-                ->withInput();
-        }
-
         return view('hello.index', ['msg' => '正しく入力されました!']);
     }
 }
--- laravelapp/app/Providers/HelloServiceProvider.php
+++ laravelapp/app/Providers/HelloServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Validator;
+use App\Http\Validators\HelloValidator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -20,9 +22,9 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer(
-            'hello.index',
-            'App\Http\Composers\HelloComposer'
-        );
+        $validator = $this->app['validator'];
+        $validator->resolver(function($translator, $data, $rules, $messages){
+            return new HelloValidator($translator, $data , $rules, $messages);
+        });
     }
 }
