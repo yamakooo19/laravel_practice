1faf8b7f89ef47f8ec0fcbc6b6f4ccb82211583c
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-19 00:22:44 +0900

    whereRawによる条件検索

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -75,11 +75,9 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-        $name = $request->name;
-        $items = DB::table('people')
-            ->where('name', 'like', '%' . $name . '%')
-            ->orWhere('mail', 'like', '%' . $name . '%')
-            ->get();
+        $min = $request->min;
+        $max = $request->max;
+        $items = DB::table('people')->whereRaw('age >= ? and age <= ?', [$min, $max])->get();
         return view('hello.show', ['items' => $items]);
     }
 }
