efaf2b265831da022ba698f1079361fa1750a8a1
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-18 16:09:16 +0900

    DBクラス

--- laravelapp/resources/views/layouts/helloapp.blade.php
+++ laravelapp/resources/views/layouts/helloapp.blade.php
@@ -43,6 +43,18 @@
             border-bottom: solid 1px #ccc;
             color: #ccc;
         }
+
+        th {
+            background-color: #999;
+            color: fff;
+            padding: 5px 10px:
+        }
+
+        td {
+            border: solid 1px #aaa;
+            color: #999;
+            padding: 5px 10px;
+        }
     </style>
 </head>
 
