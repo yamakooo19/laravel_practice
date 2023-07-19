7c24bc569dcc0d9555450d42b5dc9f3a6a93cd91
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-19 17:01:41 +0900

    Personクラスにメソッドを追加する

--- laravelapp/app/Models/Person.php
+++ laravelapp/app/Models/Person.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    use HasFactory;
+    public function getData()
+    {
+        return $this->id . ':' . $this->name . '(' . $this->age . ')';
+    }
 }
