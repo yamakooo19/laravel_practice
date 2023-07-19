59db2a4ca3ee7f3a991edb24caf7eeffbc89302c
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-20 01:43:32 +0900

    has Many結合

--- laravelapp/app/Models/Person.php
+++ laravelapp/app/Models/Person.php
@@ -22,8 +22,8 @@ class Person extends Model
         return $this->id . ':' . $this->name . '(' . $this->age . ')';
     }
 
-    public function board()
+    public function boards()
     {
-        return $this->hasOne('App\Models\Board');
+        return $this->hasMany('App\Models\Board');
     }
 }
