f53ff97eda549277c4511c4109686fa28dcc0768
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-19 18:17:42 +0900

    Scopeクラスを作成する

--- laravelapp/app/Models/Person.php
+++ laravelapp/app/Models/Person.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
@@ -32,8 +33,6 @@ class Person extends Model
     {
         parent::boot();
 
-        static::addGlobalScope('age', function (Builder $builder) {
-            $builder->where('age', '>', 20);
-        });
+        static::addGlobalScope(new ScopePerson);
     }
 }
