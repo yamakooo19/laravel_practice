590c23efe1a4b9c496bebf583363968a108b5f13
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-20 02:00:41 +0900

    関連レコードの有無

--- laravelapp/resources/views/person/index.blade.php
+++ laravelapp/resources/views/person/index.blade.php
@@ -13,20 +13,29 @@
             <th>Person</th>
             <th>Board</th>
         </tr>
-        @foreach ($items as $item)
+        @foreach ($hasItems as $item)
             <tr>
                 <td>{{ $item->getData() }}</td>
                 <td>
-                    @if ($item->boards != null)
-                        <table width="100%">
-                            @foreach ($item->boards as $obj)
-                                <tr>
-                                    <td>{{ $obj->getData() }}</td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    @endif
+                    <table width="100%">
+                        @foreach ($item->boards as $obj)
+                            <tr>
+                                <td>{{ $obj->getData() }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
                 </td>
+            </tr>
+        @endforeach
+    </table>
+    <div style="margin:10px;"></div>
+    <table>
+        <tr>
+            <th>Person</th>
+        </tr>
+        @foreach ($noItems as $item)
+            <tr>
+                <td>{{ $item->getData() }}</td>
             </tr>
         @endforeach
     </table>
