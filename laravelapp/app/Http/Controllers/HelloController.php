6c73e481e21cef3b9428e8dc69baf45269af6fe3
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-22 02:50:13 +0900

    paginateメソッドの利用

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -16,7 +16,7 @@ class HelloController extends Controller
     {
         $sort = $request->sort;
         //$items = DB::table('people')->orderBy($sort,'asc')->simplePaginate(5);
-        $items = Person::orderBy($sort, 'asc')->simplePaginate(5);
+        $items = Person::orderBy($sort, 'asc')->paginate(5);
         $param = ['items' => $items, 'sort' => $sort];
         return view('hello.index', $param);
     }
