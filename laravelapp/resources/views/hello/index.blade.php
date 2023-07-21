0ac1997e5768f8bcee5b81f268f8d1c1cfa19637
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-22 02:45:26 +0900

    ソート順を変更する

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -28,6 +28,22 @@
         .pagination li {
             display: inline-block
         }
+
+        tr th a:link {
+            color: white;
+        }
+
+        tr th a:visited {
+            color: white;
+        }
+
+        tr th a:hover {
+            color: white;
+        }
+
+        tr th a:active {
+            color: white;
+        }
     </style>
 
     @section('title', 'Index')
@@ -40,9 +56,9 @@
     @section('content')
         <table>
             <tr>
-                <th>Name</th>
-                <th>Mail</th>
-                <th>Age</th>
+                <th><a href="/hello?sort=name">name</a></th>
+                <th><a href="/hello?sort=mail">mail</a></th>
+                <th><a href="/hello?sort=age">age</a></th>
             </tr>
             @foreach ($items as $item)
                 <tr>
@@ -52,7 +68,7 @@
                 </tr>
             @endforeach
         </table>
-        {{ $items->links() }}
+        {{ $items->appends(['sort' => $sort])->links() }}
     @endsection
 
     @section('footer')
